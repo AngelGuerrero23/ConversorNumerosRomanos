9480b18893bdc70c1032115f4b97238ea01e40aa
angel <angel_guerrero1@ucne.edu.do>
2025-09-01 23:46:39 -0400

    Agregar archivos de proyecto.

---
+++ ConversorNumerosRomanos/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConversorNumerosRomanos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class Conversion()
+        {
+
+        }
+    }
+}
