46bf3c4757a989fccd1ac3ac6d5ba639abd9a25d
angel <angel_guerrero1@ucne.edu.do>
2025-09-02 00:21:30 -0400

    Dictionary

--- ConversorNumerosRomanos/Program.cs
+++ ConversorNumerosRomanos/Program.cs
@@ -4,12 +4,21 @@
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> numeroRomanos = new Dictionary<string, int>();
+            numeroRomanos.Add("I", 1);
+            numeroRomanos.Add("V", 5);
+            numeroRomanos.Add("X", 10);
+            numeroRomanos.Add("L", 50);
+            numeroRomanos.Add("C", 100);
+            numeroRomanos.Add("D", 500);
+            numeroRomanos.Add("M", 1000);
+
+            int numeroEntrante;
+
             
-        }
 
-        class Conversion()
-        {
 
         }
+
     }
 }
